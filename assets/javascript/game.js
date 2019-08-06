8cd97abcf3e404dfcdbecbad7ef50b3cac5227b6
William Gonzales <wgonz94@yahoo.com>
2019-08-05 17:38:24 -0700

    added basic structure for html

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var wins = 0;
+var losses = 0;
+var guessesLeft= 9;
+
+
+
